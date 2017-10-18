90443f918a8f7ae64258358702d28dd697b54a5f
bsstahl <barry@bsstahl.com>
2017-10-18 15:30:23 -0700

    Add Random AI Strategy project for Chutes & Ladders (copied from the Greedy implementation) and stub its implementation.

---
+++ ChutesAndLaddersDemo/Simulation/ChutesAndLadders.Strategy.Random/Engine.cs
@@ -0,0 +1,22 @@
+﻿using ChutesAndLadders.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChutesAndLadders.Strategy.Random
+{
+    public class Engine : Interfaces.IGameStrategy
+    {
+        public string Name { get => "Random"; }
+
+        public int WinCount { get; set; }
+
+
+        public int GetMove(GameSituation situation)
+        {
+            return situation.LegalMoves.GetRandom();
+        }
+    }
+}
---
+++ ChutesAndLaddersDemo/Simulation/ChutesAndLadders.Strategy.Random/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChutesAndLadders.Strategy.Random
+{
+    public static class Extensions
+    {
+        public static int GetRandom(this IEnumerable<int> moves)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
