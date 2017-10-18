41257e9a595d8d1c821a8847e664a2be787116df
bsstahl <barry@bsstahl.com>
2017-10-18 16:05:02 -0700

    Implement Random strategy logic for Chutes & Ladders

--- ChutesAndLaddersDemo/Simulation/ChutesAndLadders.Strategy.Random/Extensions.cs
+++ ChutesAndLaddersDemo/Simulation/ChutesAndLadders.Strategy.Random/Extensions.cs
@@ -10,7 +10,24 @@ namespace ChutesAndLadders.Strategy.Random
     {
         public static int GetRandom(this IEnumerable<int> moves)
         {
-            throw new NotImplementedException();
+            int result;
+
+            if (moves == null)
+                throw new ArgumentNullException(nameof(moves));
+
+            if (moves.Count() < 1)
+                throw new ArgumentException($"{nameof(moves)} must have at least 1 element");
+
+            if (moves.Count() == 1)
+                result = moves.First();
+            else
+            {
+                var rnd = new System.Random();
+                int index = rnd.Next(moves.Count());
+                result = moves.ToArray()[index];
+            }
+
+            return result;
         }
     }
 }
