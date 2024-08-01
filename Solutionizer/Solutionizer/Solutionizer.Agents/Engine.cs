bad4dfa25853335f7347ae69331158981ad4bac9
Barry Stahl <bss_github@bsstahl.com>
2024-08-01 08:13:13 -0700

    Initial stub of Agent-Based Solution bot

---
+++ Solutionizer/Solutionizer/Solutionizer.Agents/Engine.cs
@@ -0,0 +1,37 @@
+﻿using AutoGen;
+using AutoGen.Core;
+using AutoGen.OpenAI;
+
+namespace Solutionizer.Agents;
+
+public class Engine
+{
+
+    public async Task Execute()
+    {
+        var openAIKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY") ?? throw new Exception("Please set OPENAI_API_KEY environment variable.");
+        var gpt35Config = new OpenAIConfig(openAIKey, "gpt-3.5-turbo");
+
+        var assistantAgent = new AssistantAgent(
+            name: "assistant",
+            systemMessage: "You are an assistant that help user to do some tasks.",
+            llmConfig: new ConversableAgentConfig
+            {
+                Temperature = 0,
+                ConfigList = [gpt35Config],
+            })
+            .RegisterPrintMessage(); // register a hook to print message nicely to console
+
+        // set human input mode to ALWAYS so that user always provide input
+        var userProxyAgent = new UserProxyAgent(
+            name: "user",
+            humanInputMode: HumanInputMode.ALWAYS)
+            .RegisterPrintMessage();
+
+        // start the conversation
+        await userProxyAgent.InitiateChatAsync(
+            receiver: assistantAgent,
+            message: "Hey assistant, please do me a favor.",
+            maxRound: 10);
+    }
+}
---
+++ Solutionizer/Solutionizer/Solutionizer/Program.cs
@@ -0,0 +1,10 @@
+﻿var container = new ServiceCollection()
+    .BuildServiceProvider();
+
+// TODO: Ask the user to describe the problem
+// TODO: Ask the Snowden agent for the type of problem
+// TODO: Determine which agent to use to solve the problem
+// TODO: Ask the solver agent to determine the path to solve the problem
+// TODO: Put the user on the path
+
+
