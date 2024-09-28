59f098ba9f2d2dbab68c554771e72189525de6a0
Barry Stahl <bss_github@bsstahl.com>
2024-09-27 17:18:54 -0700

    Stub image search tool

--- RAG/Beary/Beary.Embeddings.LocalServer/Extensions/ServiceCollectionExtensions.cs
+++ RAG/Beary/Beary.Embeddings.LocalServer/Extensions/ServiceCollectionExtensions.cs
@@ -8,6 +8,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection UseLocalServerEmbeddingsModel(this IServiceCollection serviceCollection)
     {
         return serviceCollection
+            .AddHttpClient()
             .AddSingleton<IGetEmbeddings, LocalServer.Client>();
     }
 }
---
+++ RAG/Beary/Image/Program.cs
@@ -0,0 +1,30 @@
+﻿using Beary.Data.AzureAISearch.Extensions;
+using Beary.Data.Extensions;
+using Beary.Embeddings.LocalServer.Extensions;
+using Beary.Search.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Image;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+        var services = new ServiceCollection()
+            .AddSingleton<IConfiguration>(config)
+            .UseLocalServerEmbeddingsModel()
+            .UseBearySearch()
+            .UseBearyReadRepository()
+            .UseAzureAIEmbeddingsReadRepo()
+            .UseAzureAIContentReadRepo()
+            .BuildServiceProvider();
+
+        var engine = services.GetRequiredService<Beary.ImageSearch.Engine>();
+        await engine.Execute();
+    }
+}
